4e5212e3ddcd5466588b8ae381dee8901d9ccf1e
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-09-12 20:02:10 -0300

    Create index.js

---
+++ javascriptBasico/constantes/index.js
@@ -0,0 +1,19 @@
+const nome = 'Anna';
+console.log(nome);
+
+const a = 78; 
+const b = a;
+let c = b;
+let d = 100; 
+const e = d;
+
+console.log('Valor de a = ' + a);
+console.log('Valor de b = ' + b);
+console.log('Valor de c = ' + c);
+console.log('Valor de d = ' + d);
+console.log('Valor de e = ' + e);
+
+const g = 10; 
+const h = 2;
+console.log('multiplicação: ' + g * h);
+
