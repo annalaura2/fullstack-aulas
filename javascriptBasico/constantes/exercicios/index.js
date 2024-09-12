2b7c670e8ea5d47b9ce758c2cc1da236f6b1f34c
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-09-12 20:17:52 -0300

    Create index.js

---
+++ javascriptBasico/constantes/exercicios/index.js
@@ -0,0 +1,10 @@
+const nome = 'Sarah'; 
+const idade = 21; 
+const peso = 57; 
+const alturaEmMetros = 1.60; 
+let imc = 0; 
+
+
+imc = peso/(alturaEmMetros*alturaEmMetros);
+
+console.log(nome + " tem", idade + " anos,", "pesa " + peso, "KG", "e tem " + alturaEmMetros, "de altura", "imc = " + imc);
