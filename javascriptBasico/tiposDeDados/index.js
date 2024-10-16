ebb103b2abe899867416924a1d77686e7e7f4686
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-10-16 09:11:45 -0300

    Create index.js

---
+++ javascriptBasico/tiposDeDados/index.js
@@ -0,0 +1,11 @@
+const nome = 'Anna'; //string 
+const nome1 = "Anna"; // string 
+const nome2 = `Anna`; // string 
+const num = 10; 
+const num2 = 10.52;
+let nomeAluno; // undefined -> não aponta para local nenhum
+let sobrenomeAluno = null // Nulo -> não aponta para local nenhum
+const boolean = true; // boolean só aceita true ou false (valor lógico)
+
+
+console.log(typeof nome, nome);
